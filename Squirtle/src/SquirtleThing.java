155ca9dbc9231ef8408dfbda408396c03f6ccc0a
laurenko1 <lko269@students.fairmontschools.com>
2022-09-12 13:09:42 -0700

    First Commit

--- Squirtle/src/SquirtleThing.java
+++ Squirtle/src/SquirtleThing.java
@@ -1,3 +1,4 @@
+import java.io.ObjectInputStream.GetField;
 
 public class SquirtleThing extends PokeThing {
 
@@ -7,7 +8,7 @@ public class SquirtleThing extends PokeThing {
 	 * SquirtleThing Constructor
 	 * Creates a new <code>SquirtleThing</code> object.
 	 * Associated graphics file will be squirtle.png
-	 ****/
+	 */
 	public SquirtleThing()
 	{
 		super("Squirtle", "", 0);	
@@ -24,6 +25,7 @@ public class SquirtleThing extends PokeThing {
 		movementPattern();
 		
 		Gui g = getBoard().getGui();
+		squaresMoved++;
 		g.appendTextWindow("Squirtle has moved " + squaresMoved + " squares.");
 	}
 	
@@ -37,15 +39,42 @@ public class SquirtleThing extends PokeThing {
 		// Check the 'next' Location.  If there is a PokeThing or wall there, turn.
 		boolean blocked;
 		boolean stuck;
+		boolean flower;
 			
 		Location nextLoc = getDirection().getNextLocation(getLocation()); 
 		blocked = getBoard().thingAt(nextLoc) instanceof PokeThing;
 		stuck = !(nextLoc.isValid(getBoard()));
-		if (blocked || stuck)
+		flower = getBoard().thingAt(nextLoc) instanceof FlowerThing;
+		if (getBoard().thingAt(nextLoc) instanceof FlowerThing)
+		{
+			FlowerThing f = (FlowerThing) getBoard().thingAt(nextLoc);
+			getBoard().remove(f);
+			getBoard().getGui().appendTextWindow("FLOWER!");
+		}
+		if (getBoard().thingAt(nextLoc) instanceof BulbasaurThing)
+		{
+			if (getBoard().thingAt(nextLoc) instanceof FlowerThing)
+			{
+				FlowerThing f = (FlowerThing) getBoard().thingAt(nextLoc);
+				getBoard().remove(f);
+				getBoard().getGui().appendTextWindow("FLOWER!");
+			}
+			getBoard().getGui().appendTextWindow("Squirtle sees a bulbasaur!");
+			setDirection(getDirection().rightHalf());
+			move();
+			setDirection(getDirection().left());
+		}
+		if (blocked)
 		{
 			setDirection(getDirection().right());
+			Gui g = getBoard().getGui();
+			g.appendTextWindow("Squirtle sees a bulbausaur!");
+		} else if (stuck) {
+			setDirection(getDirection().right());
+			Gui g = getBoard().getGui();
+			g.appendTextWindow("Stuck");
 		}
-				
+	// Test
 		move();
 	}
 }
